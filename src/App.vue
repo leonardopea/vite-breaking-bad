d93385bffb0b0d45da1da1bc76a93fe7304ee529
leonardopea <leonardo.pea.1996@gmail.com>
2023-06-28 23:16:16 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script>
+
+import AppHeader from './components/AppHeader.vue';
+import PokemonList from './components/PokemonList.vue';
+
+export default {
+  components:{
+    AppHeader,
+    PokemonList
+  }
+}
+</script>
+
+
+<template>
+  <div>
+    <AppHeader/>
+    <PokemonList/>
+  </div>
+</template>
+
+
+
+<style lang="scss" scoped>
+
+@use './styles/generals.scss' as *;
+
+</style>
