bcb84ecf7f84959013a811599bdba04044ccc384
leonardopea <leonardo.pea.1996@gmail.com>
2023-06-29 11:02:41 +0200

    AppHeader completato

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -24,10 +24,26 @@ export default {
 <style lang="scss" scoped>
 
 @use '../styles/partials/mixsins' as *;
+
     .container{
         max-width: 1180px;
         background-color: #B71B1B;
+        margin-top: 50px;
+
+
+        .row{
+            display: flex;
+
+            .col{
+                padding: 20px
+            }
+
+
+        }
+
     }
+
+
 
     .button_circle_big{
         @include bottone_circolare;
@@ -41,6 +57,7 @@ export default {
         @include bottone_piccolo;
         background: rgb(255,212,212);
         background: linear-gradient(90deg, rgba(255,212,212,1) 0%, rgba(255,140,140,1) 30%, rgba(255,55,55,1) 100%); 
+        
 
     }
 
@@ -49,6 +66,7 @@ export default {
         @include bottone_piccolo;
         background: rgb(255,212,212);
         background: linear-gradient(90deg, rgba(252,252,197,1) 0%, rgba(255,255,136,1) 30%, rgba(255,255,56,1) 100%); 
+        margin-left: 15px
 
     }
 
@@ -56,7 +74,8 @@ export default {
         @include bottone_circolare;
         @include bottone_piccolo;
         background: rgb(217,236,217);
-        background: linear-gradient(90deg, rgba(217,236,217,1) 0%, rgba(148,202,148,1) 30%, rgba(58,157,58,1) 100%); 
+        background: linear-gradient(90deg, rgba(217,236,217,1) 0%, rgba(148,202,148,1) 30%, rgba(58,157,58,1) 100%);
+        margin-left: 15px 
 
     }
 </style>
